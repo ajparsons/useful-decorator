7f906be35af965c982547f80e32b06f5de28fa8c
Alex Parsons <alex@alexparsons.co.uk>
2020-10-20 12:37:37 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'useful_decorator',
+  packages = ['useful_decorator',
+              ],
+  version = '1',
+  description = 'A set of django and generic tools',
+  author = 'Alex Parsons',
+  author_email = 'alex@alexparsons.co.uk',
+  url = 'https://github.com/ajparsons/useful_decorator', 
+  download_url = 'https://github.com/ajparsons/useful_inkleby/1', 
+  keywords = [], 
+  classifiers = [],
+)
